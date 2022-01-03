8739a7d30cbc6353178b911cb77196beb3913e07
WesGtu <WesGtu@xemaps.com>
2022-01-03 18:47:35 -0500

    Week01_17

--- js/main_scripts.js
+++ js/main_scripts.js
@@ -1,5 +1,15 @@
 
+function homePageOnLoad() {
+  populateYearInFooter();
+  populateLastUpdatedInFooter();
+}
+
 function populateYearInFooter() {
   var x = document.getElementById("content");
   x.querySelector(".currentyear").innerHTML = new Date().getFullYear();
+}
+
+function populateLastUpdatedInFooter() {
+  var x = document.getElementById("content");
+  x.querySelector(".lastupdatedate").innerHTML = document.lastModified;
 }
